a3b1c9df14b76be0722c6165dbd473d030c4703b
Daniel Fintinariu <fintinariu.daniel@gmail.com>
2024-12-04 09:24:08 +0100

    Second puzzle

--- DayTwo.h
+++ DayTwo.h
@@ -14,6 +14,11 @@ private:
 	std::ifstream stream;
 	int count{ 0 };
 
+	enum Number {
+		INCREASING,
+		DECREASING
+	};
+
 public:
 	DayTwo() {
 		stream.open("DayTwo.txt");
@@ -28,7 +33,7 @@ public:
 				data.push_back(stoi(num));
 			}
 
-			if (Safe(data)) {
+			if (SafeTwo(data)) {
 				count++;
 			}
 		}
@@ -60,5 +65,39 @@ public:
 		}
 		std::cout << std::endl;
 		return true;
+	}
+
+	bool SafeTwo(std::vector<int>& numbers) {
+		int prevResult{ 0 };
+
+		for (int i{ 0 }; i < numbers.size() - 1; i++) {
+			int result = numbers[i] - numbers[i + 1];
+
+			if (differenceInvalid(prevResult, result)) {
+
+				std::cout << "Value fluctuated." << std::endl;
+				return false;
+			}
+
+			prevResult = result;
+
+			if (isPairInvalid(numbers[i], numbers[i + 1])) {
+				std::cout << "Value not in range." << std::endl;
+				return false;
+			}
+		}
+		std::cout << std::endl;
+		return true;
+	}
+
+	bool isPairInvalid(int& numOne, int& numTwo) {
+		int difference = abs(numOne - numTwo);
+		std::cout << difference << " ";
+
+		return (difference > 3 || difference < 1);
+	}
+
+	bool differenceInvalid(int& diffOne, int& diffTwo) {
+		return (diffOne * diffTwo) < 0;
 	}
 };
